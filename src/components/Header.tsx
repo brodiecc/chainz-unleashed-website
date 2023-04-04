e20a8df31cb08679b9684c830c9ea5426e5564d6
Brodie <brodieclarke@runicostudios.com>
2023-04-04 14:52:07 +0400

    Added header

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+// const navigation = [{ name: "" }];
+
+export default function Header() {
+  return (
+    <header className="bg-stone-950 text-white">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <a href="#" className="text-2xl">
+          Chainz <span className="text-orange-300">Unleashed</span>
+        </a>
+      </nav>
+    </header>
+  );
+}
