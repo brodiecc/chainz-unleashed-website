fc41e38415130be153b140d64ac3539869cad223
Brodie C <brodieclarke97@gmail.com>
2023-05-05 18:03:30 +0400

    Added animation theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,21 @@ module.exports = {
         sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
         logo: ["var(--font-thaleah-fat)", ...defaultTheme.fontFamily.sans],
       },
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
     },
   },
   plugins: [],
