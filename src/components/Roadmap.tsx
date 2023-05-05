e25e9c4977fa4a9359141a9280e3f549d4f102a8
Brodie C <brodieclarke97@gmail.com>
2023-05-05 20:12:59 +0400

    Created Development Roadmap

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -1,21 +1,23 @@
 import { Container } from "@/components/Container";
+import { RoadmapGrid } from "./RoadmapGrid";
 
 export default function Roadmap() {
   return (
     <section
       id="Roadmap"
       aria-label="Production roadmap of the game."
-      className="relative overflow-hidden bg-orange-300 pt-20 pb-28 sm:py-32"
+      className="relative overflow-hidden pt-20 pb-28 sm:py-20 bg-gradient-to-b from-stone-950  to-orange-400 "
     >
-      <Container>
-        <div
-          id="roadmap"
-          className="max-w-2xl md:mx-auto md:text-center xl:max-w-none"
-        >
-          <h2 className="font-display text-3xl tracking-tight text-slate-950 sm:text-4xl md:text-5xl font-semibold">
+      <Container className="relative">
+        <div className="max-w-2xl md:mx-auto text-center xl:max-w-none text-white">
+          <h2 className="text-3xl tracking-tight sm:text-4xl md:text-5xl">
             Roadmap
           </h2>
+          <p className="my-6 text-lg tracking-tight text-stone-100">
+            Stay up-to-date with our development goals.
+          </p>
         </div>
+        <RoadmapGrid />
       </Container>
     </section>
   );
